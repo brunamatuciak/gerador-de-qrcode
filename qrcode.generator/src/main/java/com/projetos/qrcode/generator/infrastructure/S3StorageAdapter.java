e08258337c3d1010bfb45bb9c9ce8142d08a29a7
brunamatuciak <bruna.guedes@sptech.school>
2025-07-16 20:08:50 -0300

    feat: criaçao do docker

--- qrcode.generator/src/main/java/com/projetos/qrcode/generator/infrastructure/S3StorageAdapter.java
+++ qrcode.generator/src/main/java/com/projetos/qrcode/generator/infrastructure/S3StorageAdapter.java
@@ -18,7 +18,7 @@ public class S3StorageAdapter implements StoragePort {
 
     private final String region;
 
-    public S3StorageAdapter(@Value("${aws.region}") String region, @Value("${aws.s3.bucket-name}") String bucketName) {
+    public S3StorageAdapter(@Value("${aws.s3.region}") String region, @Value("${aws.s3.bucket-name}") String bucketName) {
         this.bucketName = bucketName;
         this.s3Client = S3Client.builder().region(Region.of(region)).build();
         this.region = region;
